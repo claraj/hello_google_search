0df82ca100c76d79e4ff2ac0df1aabb6733b09df
clara <clara@clara.com>
2017-12-06 10:41:24 -0600

    Put java code in main, not test

--- src/main/java/search/HelloSearch.java
+++ src/main/java/search/HelloSearch.java
@@ -1,3 +1,5 @@
+package search;
+
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.customsearch.Customsearch;
